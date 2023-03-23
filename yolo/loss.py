021384359b7159d7d14cca2dbf71400abb275f73
KimRass <rmx1000@yonsei.ac.kr>
2023-03-23 22:01:12 +0900

    YOLO v1 VOC2012

--- yolo/loss.py
+++ yolo/loss.py
@@ -1,2 +1,110 @@
 # References
     # https://github.com/motokimura/yolo_v1_pytorch/blob/master/loss.py
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+import cv2
+import numpy as np
+from scipy.sparse import coo_matrix
+
+
+def denormalize_array(img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
+    copied_img = img.copy()
+    copied_img *= variance
+    copied_img += mean
+    copied_img *= 255.0
+    copied_img = np.clip(a=copied_img, a_min=0, a_max=255).astype("uint8")
+    return copied_img
+
+
+def convert_tensor_to_array(tensor):
+    copied_tensor = tensor.clone().squeeze().permute((1, 2, 0)).detach().cpu().numpy()
+    copied_tensor = denormalize_array(copied_tensor)
+    return copied_tensor
+
+
+def resize_image(img, w, h):
+    resized_img = cv2.resize(src=img, dsize=(w, h))
+    return resized_img
+
+
+def get_whether_object_appear_in_each_cell(bboxes, n_grids=7):
+    bboxes["x"] = bboxes.apply(
+        lambda x: (x["xmin"] + x["xmax"]) // 2, axis=1
+    )
+    bboxes["y"] = bboxes.apply(
+        lambda x: (x["ymin"] + x["ymax"]) // 2, axis=1
+    )
+    bboxes["x_grid"] = bboxes["x"].apply(
+        lambda x: int(x / (w / n_grids))
+    )
+    bboxes["y_grid"] = bboxes["y"].apply(
+        lambda x: int(x / (h / n_grids))
+    )
+
+    rows = bboxes["y_grid"].tolist()
+    cols = bboxes["x_grid"].tolist()
+    vals = [True] * len(rows)
+    obj_i = torch.from_numpy(
+        coo_matrix((vals, (rows, cols)), shape=(n_grids, n_grids)).toarray()
+    )
+    return obj_i
+
+
+darknet = Darknet()
+yolo = YOLO(darknet=darknet, n_classes=20)
+
+new_w = 448
+new_h = 448
+h, w, _ = img.shape
+if w >= h:
+    pad_w = 0
+    pad_h = (w - h) // 2
+else:
+    pad_w = (h - w) // 2
+    pad_h = 0
+pad_w, pad_h
+tranform = T.Compose(
+    [
+        T.ToTensor(),
+        T.Pad(padding=(pad_w, pad_h)),
+        T.Resize(448),
+        T.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ]
+)
+image = tranform(img).unsqueeze(0)
+
+bboxes = parse_voc2012_xml_file(annot_dir/f"""{filename}.xml""")
+obj_i = get_whether_object_appear_in_each_cell(bboxes)
+dr = draw_bboxes(img, bboxes)
+show_image(dr)
+
+
+
+n_classes = 20
+obj_i_batch = torch.stack([obj_i] * b, axis=0).unsqueeze(1)
+
+prob_loss = output[:, -20:, ...] * obj_i_batch
+
+
+
+
+
+n_grids = 7
+h, w, _ = img.shape
+
+
+
+# bboxes["xmin"] += pad_w
+# bboxes["xmax"] += pad_w
+# bboxes["ymin"] += pad_h
+# bboxes["ymax"] += pad_h
+temp = convert_tensor_to_array(image)
+temp.shape
+
+
---
+++ yolo/parepare_voc2012.py
@@ -0,0 +1,117 @@
+from pathlib import Path
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageDraw
+import xml.etree.ElementTree as et
+import extcolors
+
+
+def load_image(img_path):
+    img_path = str(img_path)
+    img = cv2.imread(img_path, flags=cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2RGB)
+    return img
+
+
+def _convert_to_pil(img):
+    if not isinstance(img, Image.Image):
+        img = Image.fromarray(img)
+    return img
+
+
+def _convert_to_array(img):
+    img = np.array(img)
+    return img
+
+
+def show_image(img):
+    copied_img = img.copy()
+    copied_img = _convert_to_pil(copied_img)
+    copied_img.show()
+
+
+def parse_voc2012_xml_file(xml_path):
+    xtree = et.parse(xml_path)
+    xroot = xtree.getroot()
+    bboxes = pd.DataFrame(
+        [
+            [int(coord.text) for coord in label.find("bndbox")] + [label.find("name").text]
+            for label
+            in xroot
+            if label.tag == "object"
+        ],
+        columns=("xmin", "ymin", "xmax", "ymax", "label")
+    )
+    return bboxes
+
+
+def draw_bboxes(img, bboxes):
+    img_pil = _convert_to_pil(img)
+    draw = ImageDraw.Draw(img_pil)
+    for xmin, ymin, xmax, ymax in bboxes[["xmin", "ymin", "xmax", "ymax"]].values:
+        draw.rectangle(
+            xy=(xmin, ymin, xmax, ymax),
+            outline=(0, 255, 0),
+            fill=None,
+            width=2
+        )
+    return _convert_to_array(img_pil)
+
+
+def exract_all_colors_from_segmentation_map(seg_map):
+    h, w, _ = seg_map.shape
+    colors = [
+        color
+        for color, _
+        in extcolors.extract_from_image(img=_convert_to_pil(seg_map), tolerance=0, limit=w * h)[0]
+    ]
+    return colors
+
+
+def get_minimum_area_mask_bounding_rectangle(mask):
+    bool = (mask != 0)
+    nonzero_x = np.where(bool.any(axis=0))[0]
+    nonzero_y = np.where(bool.any(axis=1))[0]
+    if len(nonzero_x) != 0 and len(nonzero_y) != 0:
+        xmin = nonzero_x[0]
+        xmax = nonzero_x[-1]
+        ymin = nonzero_y[0]
+        ymax = nonzero_y[-1]
+    return xmin, ymin, xmax, ymax
+
+
+def get_bboxes_from_segmentation_map(seg_map):
+    colors = exract_all_colors_from_segmentation_map(seg_map)
+    ltrbs = [
+        get_minimum_area_mask_bounding_rectangle(
+            np.all(seg_map == np.array(color), axis=2)
+        )
+        for color
+        in colors
+        if color not in [(0, 0, 0), (224, 224, 192)]
+    ]
+    bboxes = pd.DataFrame(ltrbs, columns=("xmin", "ymin", "xmax", "ymax"))
+    return bboxes
+
+
+filenames = [line.strip() for line in open("/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/ImageSets/Segmentation/trainval.txt").readlines()]
+
+img_dir = "/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/JPEGImages"
+seg_map_dir = "/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/SegmentationObject"
+annot_dir = "/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/Annotations"
+
+img_dir = Path(img_dir)
+seg_map_dir = Path(seg_map_dir)
+annot_dir = Path(annot_dir)
+
+
+for filename in filenames:
+    img = load_image(img_dir/f"""{filename}.jpg""")
+    bboxes = parse_voc2012_xml_file(annot_dir/f"""{filename}.xml""")
+    dr = draw_bboxes(img, bboxes)
+    show_image(dr)
+    # seg_map = load_image(seg_map_dir/f"""{filename}.png""")
+
+    bboxes
+
